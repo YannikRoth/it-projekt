f9436e5d94033e8016c9b0b03c5a80548d636479
Yannik Roth <roth.yannik@hotmail.com>
2019-03-21 19:58:54 +0100

    created CSV import and added Apache common library

--- src/server/model/init/CardLoader.java
+++ src/server/model/init/CardLoader.java
@@ -1,7 +1,16 @@
 package server.model.init;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 import server.model.gameplay.Card;
 
@@ -18,30 +27,53 @@ public class CardLoader {
 	 */
 	static {
 		field_mapping.put(0, "id");
-		field_mapping.put(1, "cardName");
+		field_mapping.put(1, "cardAge");
 		field_mapping.put(2, "cardType");
+		
 	}
 	
 
-	
+	/**
+	 * This method imports the masterdata
+	 */
 	public static void importCards() {
-		
-		//loop though all the fields in the CSV
-		//true must be replaced
-		
-		CSVReader reader = new CSVReader(new FileReader("yourfile.csv"));
-		
-		Scanner scanner = new Scanner(new File("/Users/pankaj/abc.csv"));
-        scanner.useDelimiter(",");
-        while(scanner.hasNext()){
-            System.out.print(scanner.next()+"|");
-        }
-        scanner.close();
-		
-		while(true) {
-			Card c = new Card(null, null, null, 0);
+		try {
+			CSVParser parser = new CSVParserBuilder()
+				    .withSeparator(';')
+				    .withIgnoreQuotations(true)
+				    .build();
+				 
+			CSVReader csvReader = new CSVReaderBuilder(new FileReader("./resource/masterdata/testCSV.csv"))
+			    .withSkipLines(1) //first line is header line, do not import
+			    .withCSVParser(parser)
+			    .build();
+			
+			List<String[]> myEntries = csvReader.readAll();
+
+			//i is the row
+			for(int i = 0; i < myEntries.size(); i++) {
+				//j is the column
+				for(int j = 0; j < myEntries.get(i).length; j++) {
+					System.out.println(field_mapping.get(j) + ": " + myEntries.get(i)[j]);
+				}
+			}
+
+			csvReader.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
+		
+	}
+	
+	//this is just temporary, will be deleted upon implementing server logic
+	public static void main(String[] args) {
+		CardLoader.importCards();
 	}
 
 }
