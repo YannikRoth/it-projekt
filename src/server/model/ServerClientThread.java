02bd9b9cee412ffe53fbc87f4cd41e8b1448f543
Yannik Roth <roth.yannik@hotmail.com>
2019-04-06 13:26:26 +0200

    code bug fix

--- src/server/model/ServerClientThread.java
+++ src/server/model/ServerClientThread.java
@@ -3,7 +3,7 @@ package server.model;
 import java.net.Socket;
 
 public class ServerClientThread {
-	Socket socket;
+	private Socket socket;
 	public ServerClientThread(Socket socket) {
 		this.socket = socket;
 	}
