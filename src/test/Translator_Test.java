337e8509f9692363bb094a2b9cc9f72335e6dfed
aldumledore <david.2991@outlook.com>
2019-04-06 10:34:33 +0200

    Trans Test Update

--- src/test/Translator_Test.java
+++ src/test/Translator_Test.java
@@ -4,9 +4,9 @@ import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
-public class Translator {
+public class Translator_Test {
 	static Locale locale = new Locale(System.getProperty("user.language"));
-	static ResourceBundle resourceBundle = ResourceBundle.getBundle(Translator.class.getName(), locale);
+	static ResourceBundle resourceBundle = ResourceBundle.getBundle(Translator_Test.class.getName(), locale);
 	
 	public static void main(String[] args) {
 		System.out.println("Loaded resources for " + locale.getLanguage());
@@ -14,7 +14,7 @@ public class Translator {
 		System.out.println(getString("program.menu.file"));
 		
 		locale = new Locale("en");
-		resourceBundle = ResourceBundle.getBundle(Translator.class.getName(), locale);
+		resourceBundle = ResourceBundle.getBundle(Translator_Test.class.getName(), locale);
 		System.out.println("Loaded resources for " + locale.getLanguage());
 		System.out.println(getString("program.menu.file"));
 		
