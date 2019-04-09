6eaa7bbaa1a46fa01748021e598714d013511926
Yannik Roth <roth.yannik@hotmail.com>
2019-04-09 21:52:09 +0200

    part 1 of board loader and world wonder creation

--- src/server/model/gameplay/Board.java
+++ src/server/model/gameplay/Board.java
@@ -1,6 +1,12 @@
 package server.model.gameplay;
 
+import java.util.Map;
+import java.util.logging.Logger;
+
+import globals.ResourceType;
 import globals.exception.IllegalParameterException;
+import server.ServiceLocator;
+import server.model.init.BoardLoader;
 
 /**
  * This is the comment
@@ -8,16 +14,142 @@ import globals.exception.IllegalParameterException;
  *
  */
 public class Board {
+	private Logger logger = ServiceLocator.getLogger();
+	/**
+	 * Stores world wonder whereas index 0 is world wonder 1
+	 * If null, no world wonder available
+	 */
+	private WorldWonder[] worldWonders = new WorldWonder[4];
 	
-	private WorldWonder[] worldWonders = new WorldWonder[3];
+	private int boardID;
+	private String boardSide;
+	private String boardName;
+	private ResourceType producingResource;
 	
 	/**
 	 * 
 	 * @param w
 	 * @throws IllegalParameterException this is a test
 	 */
+	@Deprecated
 	public Board(WorldWonder[] w) throws IllegalParameterException {
 		worldWonders = w;
+	}
+	
+	public Board(String[] values) {
+		Map<Integer, String> mapping = BoardLoader.getFieldMapping();
+		
+		//first, init the world wonders
+		this.worldWonders[0] = new WorldWonder();
+		this.worldWonders[1] = new WorldWonder();
+		this.worldWonders[2] = new WorldWonder();
+		this.worldWonders[3] = new WorldWonder();
+		
+		for(int i = 0; i< values.length; i++) {
+			String fieldName = mapping.get(i);
+			
+			if(fieldName.equals("id")) {
+				this.boardID = Integer.parseInt(values[i]);
+				continue;
+			}
+			if(fieldName.equals("side")) {
+				this.boardSide = values[i];
+				continue;
+			}
+			if(fieldName.equals("boardName")) {
+				this.boardName = values[i];
+				continue;
+			}
+			if(fieldName.equals("producingResource")) {
+				this.producingResource = ResourceType.valueOf(values[i]);
+				continue;
+			}
+			
+			//filling the worldwonders
+			if(fieldName.substring(0, 6).equals("stage1")) {
+				String temp = fieldName.substring(6);
+				if(temp.substring(0,4).equals("Cost") || temp.substring(0, 6).equals("Reward")) {
+					initWonderStageMaps(1, temp.toLowerCase(), values[i]);
+				}
+			}
+			if(fieldName.substring(0, 6).equals("stage2")) {
+				String temp = fieldName.substring(6);
+				if(temp.substring(0,4).equals("Cost") || temp.substring(0, 6).equals("Reward")) {
+					initWonderStageMaps(2, temp.toLowerCase(), values[i]);
+				}
+			}
+			if(fieldName.substring(0, 6).equals("stage3")) {
+				String temp = fieldName.substring(6);
+				if(temp.substring(0,4).equals("Cost") || temp.substring(0, 6).equals("Reward")) {
+					initWonderStageMaps(3, temp.toLowerCase(), values[i]);
+				}
+			}
+			if(fieldName.substring(0, 6).equals("stage4")) {
+				String temp = fieldName.substring(6);
+				if(temp.substring(0,4).equals("Cost") || temp.substring(0, 6).equals("Reward")) {
+					initWonderStageMaps(4, temp.toLowerCase(), values[i]);
+				}
+			}
+		}
+	}
+	
+	private void initWonderStageMaps(int i, String field, String amount) {
+		if(!amount.equals("null")) {
+			if (field.equals("costincoins")) {
+				this.worldWonders[i - 1].cost.put(ResourceType.COIN, Integer.parseInt(amount));
+			}
+			if (field.equals("costinziegel")) {
+				this.worldWonders[i - 1].cost.put(ResourceType.BRICK, Integer.parseInt(amount));
+			}
+			if (field.equals("costinerz")) {
+				this.worldWonders[i - 1].cost.put(ResourceType.ORE, Integer.parseInt(amount));
+			}
+			if (field.equals("costinholz")) {
+				this.worldWonders[i - 1].cost.put(ResourceType.WOOD, Integer.parseInt(amount));
+			}
+			if (field.equals("costinstein")) {
+				this.worldWonders[i - 1].cost.put(ResourceType.STONE, Integer.parseInt(amount));
+			}
+			if (field.equals("costinglas")) {
+				this.worldWonders[i - 1].cost.put(ResourceType.GLAS, Integer.parseInt(amount));
+			}
+			if (field.equals("costinstoff")) {
+				this.worldWonders[i - 1].cost.put(ResourceType.FABRIC, Integer.parseInt(amount));
+			}
+			if (field.equals("costinpapyrus")) {
+				this.worldWonders[i - 1].cost.put(ResourceType.PAPYRUS, Integer.parseInt(amount));
+			}
+			
+			if(field.equals("rewardsziegel")) {
+				this.worldWonders[i-1].produce.put(ResourceType.BRICK, Integer.parseInt(amount));
+			}
+			if(field.equals("rewardserz")) {
+				this.worldWonders[i-1].produce.put(ResourceType.ORE, Integer.parseInt(amount));
+			}
+			if(field.equals("rewardsholz")) {
+				this.worldWonders[i-1].produce.put(ResourceType.WOOD, Integer.parseInt(amount));
+			}
+			if(field.equals("rewardsstein")) {
+				this.worldWonders[i-1].produce.put(ResourceType.STONE, Integer.parseInt(amount));
+			}
+			if(field.equals("rewardsglas")) {
+				this.worldWonders[i-1].produce.put(ResourceType.GLAS, Integer.parseInt(amount));
+			}
+			if(field.equals("rewardsstoff")) {
+				this.worldWonders[i-1].produce.put(ResourceType.FABRIC, Integer.parseInt(amount));
+			}
+			if(field.equals("rewardspapyrus")) {
+				this.worldWonders[i-1].produce.put(ResourceType.PAPYRUS, Integer.parseInt(amount));
+			}
+			if(field.equals("rewardincoins")) {
+				this.worldWonders[i-1].produce.put(ResourceType.COIN, Integer.parseInt(amount));
+			}
+			
+		}else {
+			//this worldwonder does not exist for this board type
+			this.worldWonders[i-1] = null;
+		}
+		
 	}
 	
 
--- src/server/model/init/BoardLoader.java
+++ src/server/model/init/BoardLoader.java
@@ -14,7 +14,7 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
 import server.ServiceLocator;
-import server.model.gameplay.Card;
+import server.model.gameplay.Board;
 
 public class BoardLoader {
 	
@@ -23,62 +23,117 @@ public class BoardLoader {
 	
 	static {
 		field_mapping.put(0,"id");
-		field_mapping.put(1,"boardName");
-		field_mapping.put(2,"producingRessource");
-		field_mapping.put(3,"stage1CostinCoins");
-		field_mapping.put(4,"stage1CostinZiegel");
-		field_mapping.put(5,"stage1CostinErz");
-		field_mapping.put(6,"stage1CostinHolz");
-		field_mapping.put(7,"stage1CostinStein");
-		field_mapping.put(8,"stage1CostinGlas");
-		field_mapping.put(9,"stage1CostinStoff");
-		field_mapping.put(10,"stage1CostinPapyrus");
-		field_mapping.put(11,"stage1RewardsRawMaterial");
-		field_mapping.put(12,"stage1RewardsBasicMaterial");
-		field_mapping.put(13,"stage1RewardinPoints");
-		field_mapping.put(14,"stage1RewardFreecard");
-		field_mapping.put(15,"stage1RewardinMilitary");
-		field_mapping.put(16,"stage1RewardsFreeCardperAge");
-		field_mapping.put(17,"stage1RewardsSciencePoint");
-		field_mapping.put(18,"stage1StopsDiscardingCards");
-		field_mapping.put(19,"stage1AllowsTradeRawMaterial");
-		field_mapping.put(20,"stage1PointsForPurpleNeigbourCards");
-		field_mapping.put(21,"stage2CostinCoins");
-		field_mapping.put(22,"stage2CostinZiegel");
-		field_mapping.put(23,"stage2CostinErz");
-		field_mapping.put(24,"stage2CostinHolz");
-		field_mapping.put(25,"stage2CostinStein");
-		field_mapping.put(26,"stage2CostinGlas");
-		field_mapping.put(27,"stage2CostinStoff");
-		field_mapping.put(28,"stage2CostinPapyrus");
-		field_mapping.put(29,"stage2RewardsRawMaterial");
-		field_mapping.put(30,"stage2RewardsBasicMaterial");
-		field_mapping.put(31,"stage2RewardinPoints");
-		field_mapping.put(32,"stage2RewardFreecard");
-		field_mapping.put(33,"stage2RewardinMilitary");
-		field_mapping.put(34,"stage2RewardsFreeCardperAge");
-		field_mapping.put(35,"stage2RewardsSciencePoint");
-		field_mapping.put(36,"stage2StopsDiscardingCards");
-		field_mapping.put(37,"stage2AllowsTradeRawMaterial");
-		field_mapping.put(38,"stage2PointsForPurpleNeigbourCards");
-		field_mapping.put(39,"stage3CostinCoins");
-		field_mapping.put(40,"stage3CostinZiegel");
-		field_mapping.put(41,"stage3CostinErz");
-		field_mapping.put(42,"stage3CostinHolz");
-		field_mapping.put(43,"stage3CostinStein");
-		field_mapping.put(44,"stage3CostinGlas");
-		field_mapping.put(45,"stage3CostinStoff");
-		field_mapping.put(46,"stage3CostinPapyrus");
-		field_mapping.put(47,"stage3RewardsRawMaterial");
-		field_mapping.put(48,"stage3RewardsBasicMaterial");
-		field_mapping.put(49,"stage3RewardinPoints");
-		field_mapping.put(50,"stage3RewardFreecard");
-		field_mapping.put(51,"stage3RewardinMilitary");
-		field_mapping.put(52,"stage3RewardsFreeCardperAge");
-		field_mapping.put(53,"stage3RewardsSciencePoint");
-		field_mapping.put(54,"stage3StopsDiscardingCards");
-		field_mapping.put(55,"stage3AllowsTradeRawMaterial");
-		field_mapping.put(56,"stage3PointsForPurpleNeigbourCards");
+		field_mapping.put(1,"side");
+		field_mapping.put(2,"boardName");
+		field_mapping.put(3,"producingResource");
+		field_mapping.put(4,"stage1CostinCoins");
+		field_mapping.put(5,"stage1CostinZiegel");
+		field_mapping.put(6,"stage1CostinErz");
+		field_mapping.put(7,"stage1CostinHolz");
+		field_mapping.put(8,"stage1CostinStein");
+		field_mapping.put(9,"stage1CostinGlas");
+		field_mapping.put(10,"stage1CostinStoff");
+		field_mapping.put(11,"stage1CostinPapyrus");
+		field_mapping.put(12,"stage1RewardsZiegel");
+		field_mapping.put(13,"stage1RewardsErz");
+		field_mapping.put(14,"stage1RewardsHolz");
+		field_mapping.put(15,"stage1RewardsStein");
+		field_mapping.put(16,"stage1RewardsGlas");
+		field_mapping.put(17,"stage1RewardsStoff");
+		field_mapping.put(18,"stage1RewardsPapyrus");
+		field_mapping.put(19,"stage1RewardinCoins");
+		field_mapping.put(20,"stage1RewardinPoints");
+		field_mapping.put(21,"stage1RewardFreecard");
+		field_mapping.put(22,"stage1RewardinMilitary");
+		field_mapping.put(23,"stage1RewardsFreeCardperAge");
+		field_mapping.put(24,"stage1RewardsScienceSchriften");
+		field_mapping.put(25,"stage1RewardsScienceKompass");
+		field_mapping.put(26,"stage1RewardsScienceMeter");
+		field_mapping.put(27,"stage1StopsDiscardingCards");
+		field_mapping.put(28,"stage1AllowsTradeRawMaterial");
+		field_mapping.put(29,"stage1AllowsCopyPurpleCardOfNeighbour");
+		field_mapping.put(30,"stage2CostinCoins");
+		field_mapping.put(31,"stage2CostinZiegel");
+		field_mapping.put(32,"stage2CostinErz");
+		field_mapping.put(33,"stage2CostinHolz");
+		field_mapping.put(34,"stage2CostinStein");
+		field_mapping.put(35,"stage2CostinGlas");
+		field_mapping.put(36,"stage2CostinStoff");
+		field_mapping.put(37,"stage2CostinPapyrus");
+		field_mapping.put(38,"stage2RewardsZiegel");
+		field_mapping.put(39,"stage2RewardsErz");
+		field_mapping.put(40,"stage2RewardsHolz");
+		field_mapping.put(41,"stage2RewardsStein");
+		field_mapping.put(42,"stage2RewardsGlas");
+		field_mapping.put(43,"stage2RewardsStoff");
+		field_mapping.put(44,"stage2RewardsPapyrus");
+		field_mapping.put(45,"stage2RewardinCoins");
+		field_mapping.put(46,"stage2RewardinPoints");
+		field_mapping.put(47,"stage2RewardFreecard");
+		field_mapping.put(48,"stage2RewardinMilitary");
+		field_mapping.put(49,"stage2RewardsFreeCardperAge");
+		field_mapping.put(50,"stage2RewardsScienceSchriften");
+		field_mapping.put(51,"stage2RewardsScienceKompass");
+		field_mapping.put(52,"stage2RewardsScienceMeter");
+		field_mapping.put(53,"stage2StopsDiscardingCards");
+		field_mapping.put(54,"stage2AllowsTradeRawMaterial");
+		field_mapping.put(55,"stage2AllowsCopyPurpleCardOfNeighbour");
+		field_mapping.put(56,"stage3CostinCoins");
+		field_mapping.put(57,"stage3CostinZiegel");
+		field_mapping.put(58,"stage3CostinErz");
+		field_mapping.put(59,"stage3CostinHolz");
+		field_mapping.put(60,"stage3CostinStein");
+		field_mapping.put(61,"stage3CostinGlas");
+		field_mapping.put(62,"stage3CostinStoff");
+		field_mapping.put(63,"stage3CostinPapyrus");
+		field_mapping.put(64,"stage3RewardsCoins");
+		field_mapping.put(65,"stage3RewardsZiegel");
+		field_mapping.put(66,"stage3RewardsErz");
+		field_mapping.put(67,"stage3RewardsHolz");
+		field_mapping.put(68,"stage3RewardsStein");
+		field_mapping.put(69,"stage3RewardsGlas");
+		field_mapping.put(70,"stage3RewardsStoff");
+		field_mapping.put(71,"stage3RewardsPapyrus");
+		field_mapping.put(72,"stage3RewardinCoins");
+		field_mapping.put(73,"stage3RewardinPoints");
+		field_mapping.put(74,"stage3RewardFreecard");
+		field_mapping.put(75,"stage3RewardinMilitary");
+		field_mapping.put(76,"stage3RewardsFreeCardperAge");
+		field_mapping.put(77,"stage3RewardsScienceSchriften");
+		field_mapping.put(78,"stage3RewardsScienceKompass");
+		field_mapping.put(79,"stage3RewardsScienceMeter");
+		field_mapping.put(80,"stage3StopsDiscardingCards");
+		field_mapping.put(81,"stage3AllowsTradeRawMaterial");
+		field_mapping.put(82,"stage3AllowsCopyPurpleCardOfNeighbour");
+		field_mapping.put(83,"stage4CostinCoins");
+		field_mapping.put(84,"stage4CostinZiegel");
+		field_mapping.put(85,"stage4CostinErz");
+		field_mapping.put(86,"stage4CostinHolz");
+		field_mapping.put(87,"stage4CostinStein");
+		field_mapping.put(88,"stage4CostinGlas");
+		field_mapping.put(89,"stage4CostinStoff");
+		field_mapping.put(90,"stage4CostinPapyrus");
+		field_mapping.put(91,"stage4RewardsCoins");
+		field_mapping.put(92,"stage4RewardsZiegel");
+		field_mapping.put(93,"stage4RewardsErz");
+		field_mapping.put(94,"stage4RewardsHolz");
+		field_mapping.put(95,"stage4RewardsStein");
+		field_mapping.put(96,"stage4RewardsGlas");
+		field_mapping.put(97,"stage4RewardsStoff");
+		field_mapping.put(98,"stage4RewardsPapyrus");
+		field_mapping.put(99,"stage4RewardinCoins");
+		field_mapping.put(100,"stage4RewardinPoints");
+		field_mapping.put(101,"stage4RewardFreecard");
+		field_mapping.put(102,"stage4RewardinMilitary");
+		field_mapping.put(103,"stage4RewardsFreeCardperAge");
+		field_mapping.put(104,"stage4RewardsScienceSchriften");
+		field_mapping.put(105,"stage4RewardsScienceKompass");
+		field_mapping.put(106,"stage4RewardsScienceMeter");
+		field_mapping.put(107,"stage4StopsDiscardingCards");
+		field_mapping.put(108,"stage4AllowsTradeRawMaterial");
+		field_mapping.put(109,"stage4AllowsCopyPurpleCardOfNeighbour");
+
+
 	}
 	
 	public static void importBoards() {
@@ -104,6 +159,7 @@ public class BoardLoader {
 					//TODO Card objects have to be created here
 				}
 				
+				Board b = new Board(myEntries.get(i));
 				//TODO create board obj.
 				//Card c = new Card(myEntries.get(i));
 				//logger.info("Card name " + c.getCardName());
@@ -120,6 +176,18 @@ public class BoardLoader {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Jusrt temporary to test the importer
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BoardLoader.importBoards();
+	}
+
+	public static Map<Integer, String> getFieldMapping() {
+		return field_mapping;
 	}
 
 }
