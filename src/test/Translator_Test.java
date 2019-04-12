448f5c52d0cd6b7cd13291f4d09c1cae70589dff
aldumbledore <david.2991@outlook.com>
2019-04-12 14:34:25 +0200

    Test Translator

--- src/test/Translator_Test.java
+++ src/test/Translator_Test.java
@@ -1,5 +1,9 @@
 package test;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
@@ -8,9 +12,17 @@ public class Translator_Test {
 	static Locale locale = new Locale(System.getProperty("user.language"));
 	static ResourceBundle resourceBundle = ResourceBundle.getBundle(Translator_Test.class.getName(), locale);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		System.out.println("Loaded resources for " + locale.getLanguage());
 		
+		String currentDir = System.getProperty("user.dir");
+        System.out.println("Working Directory = " + currentDir);
+		
+        File file = new File(currentDir);
+        URL[] urls = {file.toURI().toURL()};
+        ClassLoader loader = new URLClassLoader(urls);
+        ResourceBundle rb = ResourceBundle.getBundle("server.Translator_Test", locale, loader);
+        
 		System.out.println(getString("program.menu.file"));
 		
 		locale = new Locale("en");
